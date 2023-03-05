007bb579469eea2df25750efe6529d09b9e1dd85
kiki-kanri <a470666@gmail.com>
2023-03-06 01:46:13 +0800

    Edit nuxt config.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,60 @@
+import removeConsole from 'vite-plugin-remove-console';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', href: '/favicon.svg' },
+				{ rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+			],
+			noscript: [
+				{ children: 'Javascript is required.' }
+			],
+			title: ''
+		},
+		keepalive: true
+	},
+	compression: {
+		viteCompression: {
+			algorithm: 'gzip',
+			threshold: 513
+		}
+	},
+	experimental: {
+		payloadExtraction: false
+	},
+	modules: [
+		'@averjs/nuxt-compression',
+		'@vueuse/nuxt',
+		'nuxt-purgecss'
+	],
+	purgecss: {
+		enabled: true,
+		safelist: {
+			deep: [],
+			standard: [
+				'body',
+				'html'
+			]
+		}
+	},
+	vite: {
+		build: {
+			chunkSizeWarningLimit: 1024,
+			manifest: false,
+			ssr: false
+		},
+		plugins: [
+			removeConsole()
+		],
+		server: {
+			hmr: {
+				clientPort: 443,
+				path: '/hmrws',
+				port: 12101,
+				protocol: 'wss'
+			}
+		}
+	}
+});
